342f1207baaeb0ad069491e585d22cd38b87c570
tkaro1 <91767114+tkaro1@users.noreply.github.com>
2023-11-10 21:37:55 -0800

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -12,7 +12,7 @@ terraform {
 }
 
 provider "kubernetes" {
-  version     = "~> 1.10"
+  version     = ">= 2.0.0"
 
   load_config_file = module.eks.kubeconfig_filename != "" ? true : false
   config_path      = module.eks.kubeconfig_filename
